2a764d323cda14f0ae8a14cd550fc337ac1c5b32
anilreyhan <anilreyhan@gmail.com>
2017-09-27 13:08:03 +0300

    Last changes

--- app/src/main/java/com/anilreyhan/unpixelate/Settings.java
+++ app/src/main/java/com/anilreyhan/unpixelate/Settings.java
@@ -1,12 +1,16 @@
 package com.anilreyhan.unpixelate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     CheckBox soundCB, vibrationCB;
     GameScreen gameScreen;
     Preferences preferences;
@@ -21,7 +25,6 @@ public class Settings extends AppCompatActivity {
 
         gameScreen = new GameScreen();
         preferences = new Preferences();
-
 
 
         vibrationCB.setOnClickListener(new View.OnClickListener() {
@@ -40,8 +43,28 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+        Spinner spinner = (Spinner) findViewById(R.id.difficultySpinner);
+        spinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.difficulty_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
     @Override
